a84636c77b9b1e78a7d72e02912d12c3c112524c
Chinmay Jain <138040675+Chinmayltp@users.noreply.github.com>
2024-02-09 11:34:34 +0530

    Update MyOrder.js

--- src/app/screens/MyOrder.js
+++ src/app/screens/MyOrder.js
@@ -6,7 +6,7 @@ export default function MyOrder() {
   const [orderData, setorderData] = useState({});
 
   const fetchMyOrder = async () => {
-    await fetch("http://localhost:5000/api/myOrderData", {
+    await fetch("http://merncapastoneproject-nwsg.onrender.com/api/myOrderData", {
       mode: "cors",
       method: "POST",
       headers: {
